ebe0ff3fbb91a5e3116983fc0997f3cb6728fbbb
PythonProjectDeveloper <SleepyDevil@protonmail.com>
2019-05-24 08:34:27 +0300

    feat: implement partialUsingArguments

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -150,8 +150,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+    return function() {
+        return fn(...args, ...arguments);
+    }
 }
 
 
